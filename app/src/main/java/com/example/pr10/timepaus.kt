51bccbfbe3134fcb0d9297b9c5853e103e4bc75c
GoryachevaValelia <goryachevav01@mail.ru>
2021-06-05 00:19:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pr10/timepaus.kt
@@ -0,0 +1,11 @@
+package com.example.pr10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class timepaus : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timepaus)
+    }
+}
